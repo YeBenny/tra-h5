bac0e9ed4685933543084e80a3e7771ac52efd6f
BennyYe <yeshinebenny@gmail.com>
2023-09-18 17:59:18 +0800

    Install pinia-plugin-persistedstate

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import pinia from './pinia'
 import router from './router'
 import vuetify from './vuetify'
 import i18n from './i18n'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(vuetify)
 app.use(i18n)
