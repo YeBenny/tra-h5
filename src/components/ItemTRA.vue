9a7cb5b41cc131a0d9a524735c385c6a640efa0b
YE WEIBIN <yeshinebenny@gmail.com>
2023-09-26 17:35:16 +0800

    Add components/ItemTRA

---
+++ src/components/ItemTRA.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  id: {
+    type: String
+  }
+})
+</script>
+<template>
+  <v-list-item
+    :prependAvatar="`https://picsum.photos/200?random=${Date.now()}`"
+    :title="`TRA-${id}`"
+    :subtitle="$d(new Date(), 'long')"
+  ></v-list-item>
+</template>
