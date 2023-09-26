c05909f939cafe3d8be08630ce3a4f8dddeb35a4
YE WEIBIN <yeshinebenny@gmail.com>
2023-09-26 17:37:06 +0800

    Update vuetify/index.js

--- src/vuetify/index.js
+++ src/vuetify/index.js
@@ -4,20 +4,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-const myCustomLightTheme = {
-  dark: false,
-  colors: {
-    primary: '#FFBC25'
-  }
-}
-
 export default createVuetify({
   components,
-  directives,
-  theme: {
-    defaultTheme: 'myCustomLightTheme',
-    themes: {
-      myCustomLightTheme
-    }
-  }
+  directives
 })
