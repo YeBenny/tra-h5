35799f1afbfed5205207ad08351275c4b9364045
YE WEIBIN <yeshinebenny@gmail.com>
2023-09-27 14:54:51 +0800

    Add components/Loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { toRefs } from 'vue'
+
+const props = defineProps({
+  overlay: {
+    type: Boolean,
+    required: true
+  }
+})
+
+const { overlay } = toRefs(props)
+</script>
+
+<template>
+  <v-overlay :model-value="overlay" class="align-center justify-center" persistent>
+    <v-progress-circular color="primary" indeterminate size="64"></v-progress-circular>
+  </v-overlay>
+</template>
