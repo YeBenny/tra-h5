2a65ff16d9d8920b28c4b45f3a454eec0d7c9319
YE WEIBIN <yeshinebenny@gmail.com>
2023-09-27 16:40:06 +0800

    Tune UI in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,17 +17,23 @@ const onClickHome = () => {
       minHeight: '100%'
     }"
   >
-    <v-app-bar :title="title" color="primary" density="compact" fixed> </v-app-bar>
-    <main class="w-100">
-      <v-carousel class="h-100">
-        <v-carousel-item v-for="i in 6" :key="i" @click="onClickHome">
-          <LazyImage
-            class="h-100"
-            :src="`https://picsum.photos/512/1024?random=${Date.now()}`"
-            cover
-          ></LazyImage>
-        </v-carousel-item>
-      </v-carousel>
-    </main>
+    <v-app-bar :title="title" color="primary" density="compact" fixed flat> </v-app-bar>
+    <v-main class="bg-color">
+      <v-container fluid>
+        <v-row justify="center">
+          <v-col cols="12">
+            <v-carousel height="480" cycle>
+              <v-carousel-item v-for="i in 6" :key="i">
+                <LazyImage :src="`https://picsum.photos/512/1024?random=${Date.now()}`" cover>
+                </LazyImage>
+              </v-carousel-item>
+            </v-carousel>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn color="primary" @click="onClickHome">Enter</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-layout>
 </template>
