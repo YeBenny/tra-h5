931370a3daa703b36393521f29b6642d518f55c3
YE WEIBIN <yeshinebenny@gmail.com>
2023-09-27 11:57:55 +0800

    Enable https in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { resolve, dirname } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    basicSsl(),
     VueI18nPlugin({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**')
     })
