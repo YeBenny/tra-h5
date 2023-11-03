8c9a6e6e946803a8a52c6f13e041eee1fcf2b041
YE WEIBIN <yeshinebenny@gmail.com>
2023-11-03 22:19:30 +0800

    Keep alive include HomeView in router-view

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view v-slot="{ Component, route }">
-    <keep-alive>
+    <keep-alive :include="['HomeView']">
       <component :is="Component" :key="route.fullPath" />
     </keep-alive>
   </router-view>
